bf1754182a186162e82ffd93ce9da023ff2fabbc
Van Tien Dung <dungvt.24it@vku.udn.vn>
2025-12-01 23:14:02 +0700

    add send email (fixed)

---
+++ server/config/nodeMailer.js
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+// Create a transporter for SMTP
+const transporter = nodemailer.createTransport({
+  host: "smtp.example.com",
+  port: 587,
+  secure: false, // upgrade later with STARTTLS
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+const sendEmail = async ({ to, subject, body }) => {
+  const response = await transporter.sendMail({
+    from: process.env.SENDER_EMAIL,
+    to,
+    subject,
+    html: body,
+  });
+  return response;
+};
+export default sendEmail;
--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -1,5 +1,6 @@
 import Stripe from "stripe";
 import Booking from "../models/Booking.js";
+import { inngest } from "../inngest/index.js";
 export const stripeWebhooks = async (req, res) => {
   console.log("Webhook received");
   const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
@@ -37,6 +38,7 @@ export const stripeWebhooks = async (req, res) => {
           isPaid: true,
           paymentLink: "",
         });
+        await inngest.send({ name: "app/show.booked", data: { bookingId } });
         break;
       }
 
